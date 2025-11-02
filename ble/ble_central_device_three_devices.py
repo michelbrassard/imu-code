30119590fa6838190321ed4c59967b9761e321e8
Michel Brassard <michelbrassard47@gmail.com>
2025-11-02 16:22:57 +0100

    add initial file creation

--- ble/ble_central_device_three_devices.py
+++ ble/ble_central_device_three_devices.py
@@ -6,6 +6,7 @@
 """
 
 import asyncio
+import os
 import struct
 import contextlib
 from pathlib import Path
@@ -20,14 +21,13 @@ MAGNETOMETER_UUID = "2AA1"
 
 # Manually defined devices
 DEVICES = [
-    ("Right leg", "7EFA8AE7-B4F7-2803-53D0-B65EE98ECFD5"),
-    ("Left leg", "7EFA8AE7-B4F7-2803-53D0-B65EE98ECFD1"), # dummy UUIDs
-    ("Chest", "7EFA8AE7-B4F7-2803-53D0-B65EE98ECFD0") # dummy UUIDS
+    ("right_leg", "7EFA8AE7-B4F7-2803-53D0-B65EE98ECFD5"),
+    ("left_leg", "7EFA8AE7-B4F7-2803-53D0-B65EE98ECFD1"), # dummy UUIDs
+    ("chest", "7EFA8AE7-B4F7-2803-53D0-B65EE98ECFD0") # dummy UUIDS
 ]
 
 """
-   Script for connecting 3 BLE arduinos and fetching their data
-    
+    Script for connecting 3 BLE arduinos and fetching their data
     Used: GPT + https://github.com/hbldh/bleak/blob/develop/examples/two_devices.py
 """
 def get_characteristic_name_from_uuid(uuid): 
@@ -39,27 +39,26 @@ def get_characteristic_name_from_uuid(uuid):
             return "Gyroscope (deg/s)"
         case "2aa1": 
             return "Magnetometer (µT)"
-
-# TODO vidi jel bolje da je to jedan file pa da je sve sinkronizirano ili 3 fiela s standardiziranim timestampom
-def store_files_to_directory(device_body_location, data):
-    output_file = Path(f"../data/{datetime.now().replace(microsecond=0)}/{device_body_location}/_imu_data.csv")
-    output_file.parent.mkdir(exist_ok=True, parents=True)
-    output_file.write_text("FOOBAR")
         
-async def connect_to_device(uuid_address: str, device_body_location: str, lock: asyncio.Lock):
+async def connect_to_device(uuid_address: str, device_body_location: str, lock: asyncio.Lock, directory_name: str):
     try:
         async with contextlib.AsyncExitStack() as stack:
             
-            # used because establishing a connection to multiple devices can cause errors (github, TODO read more)
+            # used because establishing a connection to multiple devices can cause errors (github)
             async with lock:
                 print(f"Looking for a device on the {device_body_location.lower()} ({uuid_address})...")
-                
+                  
                 device = await BleakScanner.find_device_by_address(uuid_address)
                 if device is None:
-                    print(f"Device on the {device_body_location.lower()} ({uuid_address}) was not found.")
+                    print(f"Device on the {device_body_location.lower()} ({uuid_address}) not found.")
                     return
                     
                 client = await stack.enter_async_context(BleakClient(device))
+                
+                # create files use with open later on...
+                output_file = Path(f"data/{directory_name}/{device_body_location}_imu_data.csv")
+                output_file.parent.mkdir(exist_ok=True, parents=True)
+                output_file.write_text("Timestamp, LinearX, LinearY, LinearZ, AngularX, AngularY, AngularZ, MagnetX, MagnetY, MagnetZ")
             # release lock
                 
             def callback(characteristic: BleakGATTCharacteristic, data: bytearray) -> None:
@@ -80,10 +79,12 @@ async def connect_to_device(uuid_address: str, device_body_location: str, lock: 
 async def main():
     lock = asyncio.Lock()
     
+    directory_name = datetime.now().replace(microsecond=0)
+    
     # start multiple async functions concurrently 
     await asyncio.gather(
         *(
-            connect_to_device(uuid_address, device_body_position, lock) 
+            connect_to_device(uuid_address, device_body_position, lock, directory_name) 
             for device_body_position, uuid_address in DEVICES
         )
     )
